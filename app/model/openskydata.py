a5f220790588c5135ccdb2e83ec434629574dbad
SirVectrex <florian.schulenberg@web.de>
2022-05-06 10:29:19 +0200

    add openskies historical time function

--- app/model/openskydata.py
+++ app/model/openskydata.py
@@ -3,20 +3,50 @@ import time as t
 
 User =
 pw =
-
 # api = OpenSkyApi(User, pw)
 
+api = OpenSkyApi(User, pw)
+
+def MUC_northernrunway_curr():
+    time = t.time()
+    for n in range(10):
+        # states = api.get_states( bbox=(48.355026, 48.374598, 11.741354, 11.838376))
+        states = api.get_states(bbox=(48.355026, 48.374598, 11.741354, 11.838376))
+
+        if states is not None:
+            print(states)
+        t.sleep(1)
+        time = time - 5
+
+    return -1;
+
+def MUC_northernrunway_hist(begin, end, step):
+    range = (end - begin) / step
+    time = begin
+    for n in range(10):
+        states = api.get_states(time_secs=time, bbox=(48.355026, 48.374598, 11.741354, 11.838376))
+
+        if states is not None:
+            print(states)
+        t.sleep(1)
+        time = time - 5
+    return -1;
+
+def MUC_southernrunway():
+
+
+    return -1;
 
 def test():
     api = OpenSkyApi(User, pw)
     # bbox = (min latitude, max latitude, min longitude, max longitude)
     time = t.time()
-    for n in range(7200):
+    for n in range(10):
         #states = api.get_states( bbox=(48.355026, 48.374598, 11.741354, 11.838376))
         states = api.get_states(time_secs=time, bbox=(48.355026, 48.374598, 11.741354, 11.838376))
 
         if states is not None:
-                    print(states)b
+                    print(states)
         t.sleep(1)
         time = time - 5
     #for s in states.states:
